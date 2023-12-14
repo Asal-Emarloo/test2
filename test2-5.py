95b60612fb37e561e2597395c81fbdef9704c2d5
Asal-Emarloo <153554023+Asal-Emarloo@users.noreply.github.com>
2023-12-14 14:43:24 +0330

    Add files via upload

---
+++ test2-5.py
@@ -0,0 +1,14 @@
+
+X=int(input("Enter number of student: "))
+for i in range(X):
+    score=float(input("Enter your score: "))
+    import math
+    sum=score
+
+avg=sum/X
+  
+print("avg class:",avg)
+print("min score:",min)
+print("min score:",max)
+ 
+
